9235a03ba53f1638bcc48bf33a3046812a9b84e9
markus90souza <markus90souza@gmail.com>
2026-01-14 12:30:30 -0300

    cconfig eslint, prettier ena lefthook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from 'prettier'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      prettier
+    },
+    rules: {
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -12,7 +23,12 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    'src/generated/prisma/**',
+    'pgdata/**',
+    '.next/**',
+    'node_modules/**',
   ]),
+
 ]);
 
 export default eslintConfig;
